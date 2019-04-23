dd6b3cf4dba9c6a07ac34ac094e746dad43f93f4
Buuntu <gabriel.jabud@gmail.com>
2019-04-23 16:42:02 +0000

    Added busco script

--- run_busco.sh
+++ run_busco.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Run docker container with busco
+# Run docker container with busco on whole genome assemblies
 # Getting all the dependencies for BUSCO installed is a hassle and takes way longer than it should, so I found a Docker image that does it for you
+# Assumes your fasta file and the lineage folder downloaded from BUSCO are in the current directory
 
 input_fasta_file=$1
 output_directory=$2
