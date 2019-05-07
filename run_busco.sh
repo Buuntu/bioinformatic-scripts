ae84ebe14ec3de139df086302cd2eca272f5d5ff
Buuntu <gabriel.jabud@gmail.com>
2019-05-07 17:51:30 +0000

    Added mode option to busco script

--- run_busco.sh
+++ run_busco.sh
@@ -4,19 +4,36 @@
 # Getting all the dependencies for BUSCO installed is a hassle and takes way longer than it should, so I found a Docker image that does it for you
 # Assumes your fasta file and the lineage folder downloaded from BUSCO are in the current directory
 
-input_fasta_file=$1
-output_directory=$2
-lineage=$3 
-
 function display_format {
-    echo "Usage: run_busco.sh [input fasta file] [output directory] [lineage]"
+    echo "Usage: run_busco.sh -f [input fasta file] -o [output directory] -l [lineage] -m [transcriptome|genome]"
     exit 1
 }
 
-if [ -z ${input_fasta_file} ] || [ -z ${output_directory} ] || [ -z ${lineage} ]; then
+while getopts ":f:o:l:m:" opt; do
+  case ${opt} in
+    f ) # process option f
+        input_fasta_file=${OPTARG}
+        ;;
+    o ) # process option o
+        output_directory=${OPTARG}
+        ;;
+    l ) # process option l
+        lineage=${OPTARG}
+        ;;
+    m ) # process option m
+        mode=${OPTARG}
+        ;;
+    \? ) display_format
+        ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+if [ -z ${input_fasta_file} ] || [ -z ${output_directory} ] || [ -z ${lineage} ] || [ -z ${mode} ]; then
     display_format
 fi
 
-docker container run -d -v $(pwd):/data --name ${output_directory} vera/busco -i ${input_fasta_file} -o ${output_directory} -l ${lineage} -m genome
+docker container run -d -v $(pwd):/data --name ${output_directory} vera/busco -i ${input_fasta_file} -o ${output_directory} -l ${lineage} -m ${mode} 
 
 
