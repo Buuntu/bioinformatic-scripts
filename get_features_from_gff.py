a74469d7fe056b8323ebacae316d51c971be1c35
Buuntu <gabriel.jabud@gmail.com>
2019-09-10 18:33:06 +0000

    Woops, removed debugging code

--- get_features_from_gff.py
+++ get_features_from_gff.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+This will extract features from a gff+fasta file based on the type (such as cds, exon, gene).  It will output to STDOUT
+"""
 
 import gffutils
 import pyfaidx
@@ -8,11 +11,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-g", help="GFF3 file", required=True)
 parser.add_argument("-f", help="Fasta file", required=True)
 parser.add_argument("-F", help="Feature type [cds, exon, gene]", required=True)
-parser.add_argument("-o", help="Output file (or stdout if none)", required=False)
 args = parser.parse_args()
 
 gff_file = args.g
-print(gff_file)
 fasta_file = args.f
 feature_type = args.F
 
@@ -20,6 +21,5 @@ db = gffutils.create_db(gff_file, ':memory:')
 fasta = pyfaidx.Fasta(fasta_file)
 
 for feature in db.features_of_type(feature_type, order_by='start'):
-    breakpoint()
     print(f">{feature.id}, {feature.seqid} {feature.start}-{feature.end}[{feature.strand}]")
     print(feature.sequence(fasta))
